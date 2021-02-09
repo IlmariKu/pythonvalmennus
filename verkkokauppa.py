e54126dfddb67b21fff6aaed1683f5ac35caa220
Ilmari Kumpula <ilmari.kumpula@gmail.com>
2021-02-09 10:53:16 +0200

    Refactoring, adding wait for some things

--- verkkokauppa.py
+++ verkkokauppa.py
@@ -1,25 +1,19 @@
-import os
 import csv
 import time
-import selenium
-from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-options = Options()
-options.headless = False
+from kaynnistys import kaynnista_selain
 
-# Käynnistyspolku
-path = "/Users/ilmari/koodi/chromedriver/chromedriver"
-
-selain = Chrome(
-    executable_path=path,
-    options=options
-)
-
+##########
 ################### KOODI ALKAA ###########################
+##########
+
+selain = kaynnista_selain()
 
 selain.get("https://verkkokauppa.com") # Kirjoita osoite osoite-kenttään ja mene selaimella sinne
+
+
 
 # Hakukenttä & haku
 time.sleep(5) # Odota viisi sekuntia, että sivu latautuu
@@ -27,9 +21,10 @@ hakuboksi = "/html/body/div[1]/div[1]/header/div/nav/form/div/input" # Etsi haku
 selain.find_element_by_xpath(hakuboksi).send_keys("Macbook pro") # Etsi hakukenttä ja kirjoita hakukenttään sanat Macbook pro
 selain.find_element_by_xpath(hakuboksi).send_keys(Keys.RETURN) # Etsi hakukenttä ja paina enteriä
 
-# Ota ensimmäisen Macbookin hinta
 
-time.sleep(5) # Odota kolme sekuntia, että sivu latautuu
+
+# Ota ensimmäisen Macbookin hinta
+time.sleep(5) # Odota viisi sekuntia
 ensimmainen_hakutulos = "/html/body/div[1]/div[1]/div/div/main/div/div[6]/div/ol[1]/li[1]/div/div[1]/div/a"
 selain.find_element_by_xpath(hakuboksi).click()
 
@@ -41,9 +36,6 @@ hinta_kohta = "/html/body/div[1]/div[1]/div/div/main/section/aside/div[2]/div[1]
 lahetystiedot_kohta = "/html/body/div[1]/div[1]/div/div/main/section/aside/div[2]/div[2]/div[1]/div[1]/span"
 ekan_koneen_hinta = selain.find_element_by_xpath(hinta_kohta).text
 ekan_koneen_lahetystiedot = selain.find_element_by_xpath(lahetystiedot_kohta)
-
-
-
 
 
 selain.close()
