90a3caec7d220f6e761d89f644b39cd0eb6da2da
Ilmari Kumpula <ilmari.kumpula@gmail.com>
2021-02-09 13:31:23 +0200

    Test-script for testing smooth operation of everything

--- kaynnistys.py
+++ kaynnistys.py
@@ -9,28 +9,35 @@ from selenium.webdriver.common.keys import Keys
 class PlatformError(Exception):
     pass
 
-
 # ChromeDriver, version 88.0.4324.96
 BASE_URL = "https://chromedriver.storage.googleapis.com/88.0.4324.96/"
 MAC_CHROME = "chromedriver_mac64.zip"
 WINDOWS_CHROME = "chromedriver_mac64.zip"
 LINUX_CHROME = "chromedriver_linux64.zip"
+CHROMEDRIVER_PATH = os.getcwd() + "/asennukset/chromedriver"
 
 
 def kaynnista_selain(kaynnista_taustalla=False):
+
+    check_and_get_chromedriver()
+
     options = Options()
     options.headless = kaynnista_taustalla
 
-    #path = "/Users/ilmari/koodi/chromedriver/chromedriver"
-
     return Chrome(
-        executable_path=path,
+        executable_path=CHROMEDRIVER_PATH,
         options=options
     )
 
 
 def check_and_get_chromedriver():
-    current_path = os.getcwd()
+
+    def is_chromedriver_installed():
+        return os.path.isfile(os.getcwd() + "/asennukset/chromedriver")
+
+    if is_chromedriver_installed() is True:
+        return
+
     operating_system = platform.system()
     if operating_system == "Darwin": # MacOS
         CHROME_OS = MAC_CHROME
@@ -42,12 +49,15 @@ def check_and_get_chromedriver():
         raise PlatformError("Platform " + str(operating_system) + " is not recognized")
 
     wget.download(BASE_URL + CHROME_OS)
+    ZIP_PATH = os.getcwd() + "/" + CHROME_OS
 
-    with zipfile.ZipFile(os.getcwd() + "/" + CHROME_OS, 'r') as zip_ref:
-        zip_ref.extractall("selainosat")
+    with zipfile.ZipFile(ZIP_PATH, 'r') as zip_ref:
+        zip_ref.extractall("asennukset")
 
-    # Pura tiedosto
-    with zipfile.ZipFile("file.zip","r") as zip_ref:
-        zip_ref.extractall("targetdir")
+    os.remove(ZIP_PATH)
+
+    if operating_system == "Darwin" or operating_system == "Linux":
+        os.chmod(CHROMEDRIVER_PATH, 755) # Mac-specific, giving broad-permissions to file
+
 
 check_and_get_chromedriver()
