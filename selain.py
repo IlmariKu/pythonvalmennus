b54d1da5a6926b9143acc7b00df311d6c515c5b3
Ilmari Kumpula <ilmari.kumpula@fraktio.fi>
2021-02-15 14:59:39 +0200

    Refactor install-script

--- selain.py
+++ selain.py
@@ -6,8 +6,10 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
+
 class PlatformError(Exception):
     pass
+
 
 # ChromeDriver, version 88.0.4324.96
 BASE_URL = "https://chromedriver.storage.googleapis.com/88.0.4324.96/"
@@ -24,41 +26,38 @@ def kaynnista_selain(kaynnista_taustalla=False):
     options = Options()
     options.headless = kaynnista_taustalla
 
-    return Chrome(
-        executable_path=CHROMEDRIVER_PATH,
-        options=options
-    )
+    return Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
 
 
 def check_and_get_chromedriver():
-
     def is_chromedriver_installed():
         return os.path.isfile(os.getcwd() + "/asennukset/chromedriver")
+
+    def unzip_chromedriver(ZIP_PATH):
+        with zipfile.ZipFile(ZIP_PATH, "r") as zip_ref:
+            zip_ref.extractall("asennukset")
+        os.remove(ZIP_PATH)
 
     if is_chromedriver_installed() is True:
         return
 
     print("Asennetaan vaadittuja paketteja...")
     operating_system = platform.system()
-    if operating_system == "Darwin": # MacOS
+    if operating_system == "Darwin":  # MacOS
         CHROME_OS = MAC_CHROME
     elif operating_system == "Windows":
         CHROME_OS = WINDOWS_CHROME
-    elif operating_system == "Linux": # MacOS
+    elif operating_system == "Linux":  # MacOS
         CHROME_OS = LINUX_CHROME
     else:
         raise PlatformError("Platform " + str(operating_system) + " is not recognized")
 
     wget.download(BASE_URL + CHROME_OS)
-    ZIP_PATH = os.getcwd() + "/" + CHROME_OS
-
-    with zipfile.ZipFile(ZIP_PATH, 'r') as zip_ref:
-        zip_ref.extractall("asennukset")
-
-    os.remove(ZIP_PATH)
+    unzip_chromedriver(os.getcwd() + "/" + CHROME_OS)
 
     if operating_system == "Darwin" or operating_system == "Linux":
-        os.chmod(CHROMEDRIVER_PATH, 755) # Mac-specific, giving broad-permissions to file
+        # Mac-specific, giving broad-permissions to file
+        os.chmod(CHROMEDRIVER_PATH, 755)
 
 
 check_and_get_chromedriver()
