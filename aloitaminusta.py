67b074049defd9f0d96e3797f301380aa1a5451c
Ilmari Kumpula <ilmari.kumpula@fraktio.fi>
2021-03-05 15:56:30 +0200

    Delete unneeded files and imports from install script

--- aloitaminusta.py
+++ aloitaminusta.py
@@ -1,13 +1,9 @@
 import csv
 import time
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selain import kaynnista_selain
-
 
 selain = kaynnista_selain()
 selain.get("https://ilmariku.github.io/asennusonnistui.html")
 time.sleep(10)
 selain.close()
 selain.quit()
-
