6296c46bc220ba469677543d4b064392fd0ff1d0
Ilmari Kumpula <ilmari.kumpula@fraktio.fi>
2021-03-05 11:31:28 +0200

    Add tahan-koodataan-template

---
+++ tahan_koodataan.py
@@ -0,0 +1,25 @@
+# Second phase
+
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+from selain import kaynnista_selain
+selain = kaynnista_selain()
+
+#################################
+##########
+################### KOODI ALKAA ###########################
+##########
+#################################
+
+
+#################################
+##########
+################### KOODI LOPPUU ###########################
+##########
+#################################
+sulje_selain_lopuksi = "ei"
+if sulje_selain_lopuksi == "kylla":
+    selain.close()
+    selain.quit()
