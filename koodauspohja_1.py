b970d65b0ea7b7b2bdb99a0314e20d3a1de26935
Ilmari Kumpula <ilmari.kumpula@fraktio.fi>
2021-04-16 11:40:13 +0300

    Add address as comment, fix unnecessary parts

--- koodauspohja_1.py
+++ koodauspohja_1.py
@@ -4,6 +4,6 @@ from selenium.webdriver.common.keys import Keys
 
 from selain import kaynnista_selain
 
-tilauslomake_osoite = "https://ilmariku.github.io/robottikurssi/tilauslomake.html"
+# tilauslomakkeen osoite: https://ilmariku.github.io/robottikurssi/tilauslomake.html
 
 selain = kaynnista_selain()
--- koodauspohja_3.py
+++ koodauspohja_3.py
@@ -4,20 +4,3 @@ from selenium.webdriver.common.keys import Keys
 
 from selain import kaynnista_selain
 selain = kaynnista_selain()
-
-#################################
-##########
-################### KOODI ALKAA ###########################
-##########
-#################################
-
-
-#################################
-##########
-################### KOODI LOPPUU ###########################
-##########
-#################################
-sulje_selain_lopuksi = "kylla"
-if sulje_selain_lopuksi == "kylla":
-    selain.close()
-    selain.quit()
