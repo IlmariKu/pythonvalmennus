a058fb0362f6a3653b7d9b70e08c715174a2f611
Ilmari Kumpula <ilmari.kumpula@fraktio.fi>
2021-04-16 11:37:43 +0300

    Clean up old, non-necessary files

---
+++ koodauspohja_1.py
@@ -0,0 +1,9 @@
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+from selain import kaynnista_selain
+
+tilauslomake_osoite = "https://ilmariku.github.io/robottikurssi/tilauslomake.html"
+
+selain = kaynnista_selain()
---
+++ koodauspohja_2.py
@@ -0,0 +1,6 @@
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+from selain import kaynnista_selain
+selain = kaynnista_selain()
