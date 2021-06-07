f45d9c80bb40a10b3982f0dadba2eee42bca8612
Ilmari Kumpula <ilmari.kumpula@fraktio.fi>
2021-06-07 09:46:56 +0300

    Replace self-install with auto-installer package

--- koodauspohja_3.py
+++
@@ -1,6 +0,0 @@
-import time
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-
-from paketit import kaynnista_selain
-selain = kaynnista_selain()
--- paketit.py
+++ paketit.py
@@ -1,45 +1,35 @@
-import os
 import sys
-import platform
-import zipfile
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+
+asennukset = [
+    "selenium",
+    "ipykernel"
+    "chromedriver-autoinstaller"
+]
 
 try:
-    import wget
-    from selenium.webdriver import Chrome
+    from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.common.keys import Keys
+    import chromedriver_autoinstaller
 except ImportError:
     import subprocess
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "selenium"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "wget"])
-    subprocess.check_call(
-        [sys.executable, "-m", "pip", "install", "ipykernel"])
-    import wget
-    from selenium.webdriver import Chrome
+    for asennus in asennukset:
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", asennus])
+    from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.common.keys import Keys
+    import chromedriver_autoinstaller
 
 
 class PlatformError(Exception):
     pass
 
 
-# ChromeDriver, version 91.0.4472.19
-BASE_URL = "https://chromedriver.storage.googleapis.com/90.0.4430.24/"
-MAC_CHROME = "chromedriver_mac64.zip"
-WINDOWS_CHROME = "chromedriver_mac64.zip"
-LINUX_CHROME = "chromedriver_linux64.zip"
 ASENNUKSET_PATH = __file__.replace(
     "paketit.py", "") + "asennukset/"
-CHROMEDRIVER_PATH = ASENNUKSET_PATH + "chromedriver"
 CRX_PATH = ASENNUKSET_PATH + 'xpath.crx'
 
 
 def kaynnista_selain(kaynnista_taustalla=False):
-
-    check_and_get_chromedriver()
 
     def wrong_spelling(komennossa_on_s_liikaa=""):
         print("Valitsit komennon vahingossa väärin. Poista kirjoittamastasi komennosta -s kirjain lopusta. element, ei elements")
@@ -48,7 +38,7 @@ def kaynnista_selain(kaynnista_taustalla=False):
     options.headless = kaynnista_taustalla
     options.add_extension(CRX_PATH)
 
-    chromeselain = Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
+    chromeselain = webdriver.Chrome(options=options)
 
     chromeselain.find_elements_by_class_name = wrong_spelling
     chromeselain.find_elements_by_css_selector = wrong_spelling
@@ -62,39 +52,4 @@ def kaynnista_selain(kaynnista_taustalla=False):
     return chromeselain
 
 
-def check_and_get_chromedriver():
-    def is_chromedriver_installed():
-        return os.path.isfile(CHROMEDRIVER_PATH)
-
-    def unzip_chromedriver(ZIP_PATH):
-        try:
-            with zipfile.ZipFile(ZIP_PATH, "r") as zip_ref:
-                zip_ref.extractall(ASENNUKSET_PATH)
-        except FileNotFoundError:
-            return
-        os.remove(ZIP_PATH)
-
-    if is_chromedriver_installed() is True:
-        return
-
-    print("Asennetaan vaadittuja paketteja...")
-    operating_system = platform.system()
-    if operating_system == "Darwin":  # MacOS
-        CHROME_OS = MAC_CHROME
-    elif operating_system == "Windows":
-        CHROME_OS = WINDOWS_CHROME
-    elif operating_system == "Linux":  # MacOS
-        CHROME_OS = LINUX_CHROME
-    else:
-        raise PlatformError(
-            "Platform " + str(operating_system) + " is not recognized")
-
-    wget.download(BASE_URL + CHROME_OS, ASENNUKSET_PATH)
-    unzip_chromedriver(ASENNUKSET_PATH + "/" + CHROME_OS)
-
-    if operating_system == "Darwin" or operating_system == "Linux":
-        # Mac-specific, giving broad-permissions to file
-        os.chmod(CHROMEDRIVER_PATH, 755)
-
-
-check_and_get_chromedriver()
+chromedriver_autoinstaller.install()
