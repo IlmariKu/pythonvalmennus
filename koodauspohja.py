0dd88a848dda640830c93f21ff08965d6644c49f
Ilmari Kumpula <ilmari.kumpula@fraktio.fi>
2022-05-27 08:53:23 +0300

    Add the By-package, for using different methods

--- koodauspohja.py
+++ koodauspohja.py
@@ -2,6 +2,7 @@
 
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 from paketit import kaynnista_selain
 
